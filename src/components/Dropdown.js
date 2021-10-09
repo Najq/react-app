d7dda4b9ecbefe1622bdfbfebdd48b98fddd1b1b
najeeb <najeeb.qazi@alida.com>
2021-10-09 14:44:34 -0700

    initial commit

---
+++ src/components/Dropdown.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState, useRef } from "react";
+
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+        };
+
+        document.body.addEventListener("click", onBodyClick, { capture: true });
+
+        // this is because we want no event listeners in case the element is out of the dom structure
+        return () => {
+            document.body.removeEventListener("click", onBodyClick);
+        };
+    }, []);
+
+    const renderedOptions = options.map((option) => {
+        //remove the value from the dropdown
+        if (option.value === selected.value) {
+            return null;
+        }
+
+        return (
+            <div
+                key={option.value}
+                className="item"
+                onClick={() => onSelectedChange(option)}
+            >
+                {option.label}
+            </div>
+        );
+    });
+
+    return (
+        <div ref={ref} className="ui form">
+            <div className="field">
+                <label className="label">{label}</label>
+                <div
+                    onClick={() => setOpen(!open)}
+                    className={`ui selection dropdown ${
+                        open ? "visible active" : ""
+                    }`}
+                >
+                    <i className="dropdown icon"></i>
+                    <div className="text"> {selected.label}</div>
+                    <div className={`menu ${open ? "visible transition" : ""}`}>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Dropdown;
